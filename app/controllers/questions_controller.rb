d69edc1d1317cecfde8c9b1a18261c8db444d7d5
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-11-13 08:07:56 +0100

    ex 2 stupid Coachings is done styledone

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,9 +7,10 @@ class QuestionsController < ApplicationController
       if params[:question] === "Great!"
        @answer = "- I am going to work,"
      elsif params[:question].include?("?")
-  @answer = "get dressed and go to work!"
-else
+      @answer = "get dressed and go to work!"
+     else
        @answer ="- I don't care, get dressed and go to work!"
       end
+
  end
 end
