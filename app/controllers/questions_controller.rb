db3cb74daa5bf28ab63875c042dee989dc74b08c
lajimi mahmoud <mahmoudladjimi@gmail.com>
2023-11-13 07:14:30 +0100

    ex 2 stupid Coachings is done stu

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,11 +5,11 @@ class QuestionsController < ApplicationController
   end
   def answer
       if params[:question] === "Great!"
-       @answer = "I am going to work,"
+       @answer = "- I am going to work,"
      elsif params[:question].include?("?")
   @answer = "get dressed and go to work!"
 else
-       @answer ="I don't care, get dressed and go to work!"
+       @answer ="- I don't care, get dressed and go to work!"
       end
  end
 end
